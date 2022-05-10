065e5c844c1916f3937635c93eeafa06c5f47d60
Leonid <you-2-me@yandex.ru>
2022-05-10 16:02:04 +0300

    Change Thematic Group slug into SlugField

--- yatube/groups/models.py
+++ yatube/groups/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
